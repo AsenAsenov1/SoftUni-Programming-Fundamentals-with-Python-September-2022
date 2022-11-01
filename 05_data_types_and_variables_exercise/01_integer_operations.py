6f346b8d9b1d3ad44d11d5bb3908587484abf9f2
Asen Asenov <47159032+AsenAsenov1@users.noreply.github.com>
2022-11-01 16:56:44 +0200

    upload

---
+++ 05_data_types_and_variables_exercise/01_integer_operations.py
@@ -0,0 +1,8 @@
+first_num = int(input())
+second_num = int(input())
+third_num = int(input())
+fourth_num = int(input())
+
+total_sum = ((first_num + second_num) // third_num) * fourth_num
+
+print(total_sum)
