a9a59b9b18d98b976a336c7ed48422480ab35d78
Asen Asenov <47159032+AsenAsenov1@users.noreply.github.com>
2022-11-06 09:12:31 +0200

    Upload

---
+++ 10_functions_lab/04_repeat_string.py
@@ -0,0 +1,9 @@
+# def repeat_string(some_str):
+#     return some_str * counter
+
+repeat_str = lambda x: string_input * counter
+
+string_input = input()
+counter = int(input())
+
+print(repeat_str(string_input))
