10000f93de722052ab657a4107cf6a0673611ccf
Asen Asenov <47159032+AsenAsenov1@users.noreply.github.com>
2022-11-07 08:01:16 +0200

    Upload

---
+++ 11_functions_exercise/01_smallest_of_three_numbers.py
@@ -0,0 +1,8 @@
+def find_min_number(first_num, second_num, third_num):
+    return min(first_num, second_num, third_num)
+
+
+first_number_input = int(input())
+second_number_input = int(input())
+third_number_input = int(input())
+print(find_min_number(first_number_input,second_number_input,third_number_input))
