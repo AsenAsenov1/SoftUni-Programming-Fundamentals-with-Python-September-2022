c1a3eeebfc854bd976a49ee0a7515f6ef9643b32
Asen Asenov <47159032+AsenAsenov1@users.noreply.github.com>
2022-12-03 12:00:56 +0200

    Create 07_word_synonyms.py

---
+++ 19_dictionaries_lab/07_word_synonyms.py
@@ -0,0 +1,12 @@
+number = int(input())
+synonyms = {}
+
+for i in range(number):
+    word = input()
+    synonym = input()
+    if word not in synonyms:
+        synonyms[word] = []
+    synonyms[word].append(synonym)
+
+for word in synonyms:
+    print(f"{word} - {', '.join(synonyms[word])}")
